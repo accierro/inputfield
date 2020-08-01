44526ab4df85e2087ed426500591000b0a5d385a
Vlad <accierro@gmail.com>
2020-08-01 17:33:47 +0300

    Add storybook

---
+++ src/storybook/Input.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Input from "../components/Input";
+
+export default {
+  title: "Input",
+};
+
+export const small = () => <Input />;
---
+++ storybook-static/precache-manifest.3f5e1e94ef1b4809bd8fd7c842a7df78.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c209b8ac98d78ff8299eef88d0029a5",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ad513bde6deeda8546ea.bundle.js"
+  },
+  {
+    "url": "runtime~main.ad513bde6deeda8546ea.bundle.js"
+  },
+  {
+    "url": "vendors~main.ad513bde6deeda8546ea.bundle.js"
+  },
+  {
+    "url": "vendors~main.ad513bde6deeda8546ea.bundle.js.LICENSE.txt"
+  }
+]);
